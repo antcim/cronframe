609d0fb1b4516ab4d9a9329eff2bad96651aa346
antcim <antoniocimino.c@gmail.com>
2024-06-17 15:21:26 +0200

    rolling logger

--- cronframe/src/logger.rs
+++ cronframe/src/logger.rs
@@ -1,16 +1,32 @@
 use log4rs::{
-    append::file::FileAppender,
+    append::rolling_file::{
+        policy::compound::{
+            roll::fixed_window::FixedWindowRoller, trigger::size::SizeTrigger, CompoundPolicy,
+        },
+        RollingFileAppender,
+    },
     config::{Appender, Config, Root},
     encode::pattern::PatternEncoder,
 };
 
 pub fn default_logger() -> log4rs::Handle {
+    let window_size = 3;
+    let roller = FixedWindowRoller::builder()
+        .build("archive_log/log{}.log", window_size)
+        .unwrap();
+
+    let size_limit = 5000 * 1024;
+
+    let trigger = SizeTrigger::new(size_limit);
+
+    let policy = CompoundPolicy::new(Box::new(trigger), Box::new(roller));
+
     let pattern = "{d(%Y-%m-%d %H:%M:%S UTC%Z)} {l} {t} - {m}{n}";
 
-    let log_file = FileAppender::builder()
+    let log_file = RollingFileAppender::builder()
         .encoder(Box::new(PatternEncoder::new(pattern)))
         .append(false)
-        .build("log/cronframe.log")
+        .build("log/latest.log", Box::new(policy))
         .unwrap();
 
     let config = Config::builder()
